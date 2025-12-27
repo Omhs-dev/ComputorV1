a9d8188dc520c50db1c21a919fea1d162586c009
Omar Hamadou <hamadouomar34@gmail.com>
2025-12-27 17:22:34 +0100

    Refactor main function to improve equation normalization and term extraction

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
 from parse import *
+from regex import *
 
 def main():
 	try:
 		inputname = input("computor ")
 
-		l, r = normalize_equation(inputname)
-		print(f"left: {l} and right: {r}")
-		# parse_side(inputname)
+		l_side, r_side = normalize_equation(inputname)
+		print(f"left: {l_side} and right: {r_side}")
+
+		i = 0
+		for term in extract_raw_terms(l_side):
+			print(f"terms {i}: {term}")
+			singed_coef, exponent = extract_term_components(term)
+			print(f"singed_coef: {singed_coef}")
+			print(f"exponent: {exponent}")
+			i+=1
 	except ValueError as e:
 		print(f"Error: {e}")
 
---
+++ regex.py
@@ -0,0 +1,32 @@
+import re
+
+# 5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0
+
+# if there is no X exponent is 0
+
+#match = re.match(r"^([-+])([0-9]+(?:\.[0-9]+)?)\*X(?:\^([0-9]+))?$", "+7")
+
+def extract_raw_terms(n_str):
+	# print(f"Str received: {n_str}")
+	if not n_str:
+		return []
+	terms = re.findall(r"[+-][^+-]+", n_str)
+	return terms
+
+def extract_term_components(term):
+	match = re.match(r"^([-+])([0-9]+(?:\.[0-9]+)?)\*X(?:\^([0-9]+))?$", term)
+
+	if not match:
+		return None
+	sign, const, exp = match.groups()
+	return sign, const, exp
+
+
+def parse_only_constant(term):
+	match = re.match(r"^([-+])([0-9]+(?:\.[0-9]+)?)$", term)
+
+	if not match:
+		return None
+	exp = 1
+	sign, const = match.groups()
+	return sign, const, exp
