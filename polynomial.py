a3fad2c8889367eac0fd6ce70240c7506b1cd7b9
Omar Hamadou <hamadouomar34@gmail.com>
2025-12-30 11:51:11 +0100

    refactor: simplify reduce_equation logic by removing unnecessary checks and improving coefficient calculation

--- polynomial.py
+++ polynomial.py
@@ -14,26 +14,23 @@ def reduce_equation(left, right):
 	print(f"left dict: {left}")
 	print(f"right dict: {right}")
 
-	# l_keys = left.keys()
-	# r_keys = right.keys()
-
-	# print(f"keys l: {l_keys}")
-	# print(f"keys r: {r_keys}")
-
 	# here each matching exponent is given once because for the union
 	all_exp = set(left) | set(right)
 
 	print(f"all exp: {all_exp}")
 	for exp in all_exp:
-		l = left.get(exp)
-		r = right.get(exp)
+		l = left.get(exp, 0.0)
+		r = right.get(exp, 0.0)
 		print(f"For key: {exp} -> left: {l}, right: {r}")
-
-		if l != None and r != None:
-			l-=r
+		coef = l - r
+		
+		if abs(coef) > 1e-12:
+			left[exp] = round(coef, 12)
 			print(f"Right side goes to the left -> l - r: {l}")
+	print(f"\nupdated left dict: {left}")
+	return left
 
-left_dict = {0: 5.0, 1: 4.0, 3: 5.0}
+left_dict = {0: 5.3, 1: 4.0, 3: 5.0}
 right_dict = {0: 4.0}
 
 reduce_equation(left_dict, right_dict)
